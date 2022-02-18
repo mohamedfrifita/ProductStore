5cfb855c1d20361c3c97430c6739d2b151f681f0
Mohamed Frifita <mohamed.frifita@esprit.tn>
2022-02-18 01:09:55 +0100

    Add project files.

---
+++ MF.Service/ProductManage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MF.Domain;
+namespace MF.Service
+{
+    public delegate IList<Product> FindDelege(IList<Product> l, string lettre);
+ public   class ProductManage
+    {
+        IList<Product> products;
+        public FindDelege FindProduct { get; set; }
+        public ProductManage(IList<Product>list)
+        {
+            products = list;
+        }
+        public IList<Product>FindProductMethode(FindDelege f,string chaine)
+        {
+            return f(products, chaine);
+        }
+    }
+}
